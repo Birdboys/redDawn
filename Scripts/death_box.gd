17b53b48f9a8435ccada40a7c9c242cd0bbc0509
Birdboys <colbyjbird@comcast.net>
2023-07-26 12:54:54 -0600

    moving kinda work

---
+++ Scripts/death_box.gd
@@ -0,0 +1,16 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_entered(area):
+	area.get_parent().position = Vector3(0,1,0)
+	pass # Replace with function body.
